4bf6300b1d6fe358cf34eea96cbfdfe71063f97b
Rizk-MD <108157378+Rizk-MD@users.noreply.github.com>
2022-07-16 04:23:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -60,8 +60,8 @@ global.ppulsa2 = '0895394988123'
 global.psaweria = 'https://saweria.co/FangzBot'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '62895394988123'
-global.nomorown = '6288215689772'
+global.nomorbot = '6283150688326'
+global.nomorown = '6283150688326'
 global.namebot = ' 「 FANGZ BOT ⁩㊣ 」'
 global.nameown = 'Fangz Ganz'
 
